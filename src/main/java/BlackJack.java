a6f7cd0efbe0d03e31fa7bfdf265245b8959ac53
Stefun <hawkd215@gmail.com>
2019-06-16 22:11:35 -0400

    update

--- src/main/java/BlackJack.java
+++ src/main/java/BlackJack.java
@@ -5,61 +5,66 @@ public class BlackJack extends CardGame {
 
 Integer sumHand = 0;
 
-//    @Override
-//    void playGame() {
-//
-//    }
-//
-//    @Override
-//    void endOfGame() {
-//
-//    }
+    @Override
+    void playGame() {
+
+    }
+
+    @Override
+    void endOfGame() {
+
+    }
 
 
     public BlackJack(BlackJack_Player players, Integer numberOfDecks) {
         super(players, numberOfDecks);
-
     }
+
+
     Decks blackJackDeck = new Decks(5);
     House house = new House(2, blackJackDeck);
 
-//    public void playGame(){
-//        while(!checkWin()){
-//            house.moneyToChips(50.00);
-//            house.takeBets(2);
-//            house.shuffle();
-//            house.dealCard();
-//
-//
-//
-//        }
-//
-//    }
-//    Hand playerHand = new Hand() {
-//        @Override
-//    public Integer numberOfCardsInHand() {
-//            return super.numberOfCardsInHand();
-//        }
-//    };
 
-//    public Boolean checkWin(){
-//       if (playerHand >)
-//
-//
-//
-//        return false;
-//    }
-//
-//
-//    public Integer HandSum(Hand hand){
-//         for (int i = 0; i < 5; i++) {
-//             if(hand.card.getRank() != null){
-//
-//               sumHand  += playerHand.get(i).getValue();
-//             }
-//
-//         }return sumHand;
-//     }
-//    public Boolean didWin(Player player) {return false;}
+    public void playGame(){
+        while(!checkWin()){
+            System.out.println("Welcome to the BlackJack table. How many chips would you like?");
+            house.moneyToChips(50.00);
+            house.takeBets(2);
+            house.shuffle();
+            house.dealCard(1).isFaceUpToMe();
+            house.dealCard(1).isFaceUpToEveryone();
+
+
+
+
+        }
+
+    }
+    Hand playerHand = new Hand() {
+        @Override
+    public Integer numberOfCardsInHand() {
+            return super.numberOfCardsInHand();
+        }
+    };
+
+    public Boolean checkWin(){
+       if (playerHand.)
+
+
+
+        return false;
+    }
+
+
+    public Integer HandSum(Hand hand){
+         for (int i = 0; i < 5; i++) {
+             if(hand.card.getRank() != null){
+
+               sumHand  += playerHand.get(i).getValue();
+             }
+
+         }return sumHand;
+     }
+    public Boolean didWin(Player player) {return false;}
 
 }
--- src/main/java/BlackJack_Player.java
+++ src/main/java/BlackJack_Player.java
@@ -1,3 +1,5 @@
+import exceptions.NegativeBetException;
+
 import java.util.ArrayList;
 
 public class BlackJack_Player extends Player implements Gambling {
@@ -6,54 +8,38 @@ public class BlackJack_Player extends Player implements Gambling {
     Integer bet;
 
 
-
-
     public BlackJack_Player(Person player) {
         super(player);
     }
 
-    public Boolean didWin(){ return false;}
-    public void makeMove(){}
-
-
-    public Boolean canSplit(){
-      ArrayList<Card> playerHand =  new ArrayList<>(5);
-      if(playerHand.get(0) == playerHand.get(1)){
-          System.out.println("Would you like to split?");
-          return true;
-      }
-      return false;}
-    public void split() {
-        if (chipCount >= 2){
-            bet();
-        }
+    public Boolean didWin() {
+        return false;
     }
+
+    public void makeMove() {
+    }
+
+
+    public Boolean canSplit() {
+        ArrayList<Card> playerHand = new ArrayList<>(5);
+        if (playerHand.get(0) == playerHand.get(1)) {
+            System.out.println("Would you like to split?");
+            return true;
+        }
+        return false;
+    }
+//    public void split() {
+//        if (chipCount >= 2){
+//            bet();
+//        }
+//}
     public void hitOrStay() {}
     public void doubleDown() {}
     public void leaveGame(){}
 
+
     @Override
-    public Integer bet(Integer numOfChipsToBet) {
+    public Integer bet(Integer numOfChipsToBet) throws NegativeBetException {
         return null;
-    }
-
-    @Override
-    public Hand dealRound(Integer numberOfCards, Boolean faceDown) {
-        return null;
-    }
-
-    @Override
-    public Hand dealHand(Integer numberOfC) {
-        return null;
-    }
-
-    @Override
-    public Card dealCard(Integer numberOfCards) {
-        return null;
-    }
-
-    @Override
-    public void shuffle() {
-
     }
 }
