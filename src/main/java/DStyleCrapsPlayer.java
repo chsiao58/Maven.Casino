67ca2afc461d0173c2f6b7bedcd4c0bcfcd09a4a
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-06-17 12:51:04 -0400

    fixed craps player conflicts

--- src/main/java/DStyleCrapsPlayer.java
+++ src/main/java/DStyleCrapsPlayer.java
@@ -1,29 +1,18 @@
 public class DStyleCrapsPlayer extends Player{
-    Person person;
-    Integer chips;
-<<<<<<< HEAD
-=======
-    Integer betchips;
+    private Person person;
+    private Integer chips;
 
-
-//        House house=new House(2, null);
-//        chips=house.moneyToChips(20.0);
-
-
-
-    public  DStyleCrapsPlayer(Person player,Integer chips){
->>>>>>> ada73ca9ac4742d37a82da0884bebf26e8ff6d52
+    public  DStyleCrapsPlayer(Person player){
         this.person=player;
         House house=new House(2, null);
-        chips=house.moneyToChips(20.0);
-
+        this.chips = house.moneyToChips(20.0);
     }
 
     public Integer roll(Dice dice){
        Integer total= dice.tossAndSum();
         return total;
     }
-<<<<<<< HEAD
+
     public Integer numOfChips(){
         if (person.getWallet()==0)
             return null;
@@ -31,8 +20,6 @@ public class DStyleCrapsPlayer extends Player{
         return chips;
         
     }
-=======
->>>>>>> ada73ca9ac4742d37a82da0884bebf26e8ff6d52
     public Integer bet(Integer betChips){
         chips=chips-betChips;
         return betChips;
