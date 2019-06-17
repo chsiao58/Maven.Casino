bc0eef2c146c993a864054552547dd3e95bb800b
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-06-17 13:22:06 -0400

    blackJack should compile

--- src/main/java/BlackJack.java
+++ src/main/java/BlackJack.java
@@ -1,29 +1,28 @@
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class BlackJack extends CardGame {
-    ArrayList<Card> playerHand;
-    ArrayList<Card> houseHand;
-    private Decks blackJackDeck;
+    private BlackJackHand playerHand;
+    private BlackJackHand dealerHand;
     private Integer chipCount;
     private House house;
-    private Scanner input;
+    private Scanner input = new Scanner(System.in);
+    private Boolean gameOver;
 
     public BlackJack(BlackJack_Player players, Integer numberOfDecks) {
         super(players, numberOfDecks);
-        this.playerHand = new ArrayList<>(5);
-        this.houseHand = new ArrayList<>(5);
-        this.blackJackDeck = new Decks(5);
+        this.playerHand = new BlackJackHand(new ArrayList<>(5));
+        this.dealerHand = new BlackJackHand(new ArrayList<>(5));
+        Decks blackJackDeck = new Decks(5);
         this.chipCount = 0;
         this.house = new House(2, blackJackDeck);
-        this.input = new Scanner(System.in);
+        this.gameOver = false;
     }
 
 
     public void playGame() {
-        while (!checkWin()) {
+        while (!gameOver) {
             System.out.println("Welcome to the BlackJack table.");
             input.nextInt(Integer.parseInt("How many chips would you like?"));
             Chips playerChips = new Chips(house.moneyToChips(50.00) / 5);
@@ -34,29 +33,36 @@ public class BlackJack extends CardGame {
 
             house.shuffle();
 
-            house.dealCard(1).isFaceUpToMe();
-            playerHand.getCards().add();
+            Card card = house.dealCard();
+            card.setFaceUpToMe();
+            playerHand.addCard(card);
 
-            house.dealCard(1).isFaceDownToEveryone();
-            dealerHand.addCard();
+            Card card2 = house.dealCard();
+            card2.setFaceDownToEveryone();
+            playerHand.addCard(card2);
 
-            house.dealCard(1).isFaceUpToEveryone();
-            playersHand.add();
+            Card card3 = house.dealCard();
+            card3.setFaceUpToEveryone();
+            playerHand.addCard(card3);
 
+            Card card4 = house.dealCard();
+            card4.setFaceUpToEveryone();
+            dealerHand.addCard(card4);
 
-
-            house.dealCard(1).isFaceUpToEveryone();
-            dealerHand.addCard();
             checkWin();
+
             input.nextInt(Integer.parseInt("Would you like to Hit?"));
 
 
-            house.dealCard(1).isFaceUpToEveryone();
+            Card card5 = house.dealCard();
+            card5.setFaceUpToEveryone();
+            dealerHand.addCard(card5);
             checkWin();
             input.nextInt(Integer.parseInt("Would like another hit or would you like to stay?"));
 
         }
     }
+
 
 
     void endOfGame() {
@@ -65,22 +71,22 @@ public class BlackJack extends CardGame {
 
     public String checkWin() {
         if (playerHand.sumHand() > 21) {
+            gameOver = true;
             return "Awww...too bad. You BUSTED!";
         }
-
-        if (playerHand.sumHand() == dealerHand.sumHand()) {
-
+        if (playerHand.sumHand().equals(dealerHand.sumHand())) {
             return "All tied up...it's a push.";
         }
         if (playerHand.sumHand() > dealerHand.sumHand() && playerHand.sumHand() <= 21) {
+            gameOver = true;
             return "Congratulations Player 1, you've won!";
         }
+        return "You shouldn't be here!";
+    }
 
 
-
-
-    }public Boolean canSplit() {
-        if (playerHand.get(0) == playerHand.get(1)) {
+    public Boolean canSplit() {
+        if (playerHand.getCards().get(0) == playerHand.getCards().get(1)) {
             System.out.println("Would you like to split?");
             return true;
         }
--- src/main/java/BlackJackHand.java
+++ src/main/java/BlackJackHand.java
@@ -1,9 +1,6 @@
 import java.util.ArrayList;
 
 public class BlackJackHand extends Hand {
-    public BlackJackHand(ArrayList<Card> cards) {
-        super(cards);
-    }
 
 
     public BlackJackHand(ArrayList<Card> cards) {
